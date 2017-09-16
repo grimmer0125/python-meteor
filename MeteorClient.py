8fa926c9f4ae4d5b0ce750c2412701584cb2226a
Grimmer <grimmer0125@gmail.com>
2017-09-17 03:22:51 +0800

    Encode password string before hashing. This is needed for Python 3 and compatible with Python 2.

--- MeteorClient.py
+++ MeteorClient.py
@@ -114,7 +114,7 @@ class MeteorClient(EventEmitter):
         #       we need to authenticate
 
         # hash the password
-        hashed = hashlib.sha256(password).hexdigest()
+        hashed = hashlib.sha256(password.encode('utf-8')).hexdigest()
         # handle username or email address
         if '@' in user:
             user_object = {
